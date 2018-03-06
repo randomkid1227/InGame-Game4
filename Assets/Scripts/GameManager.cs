8b05cb07bf88fd73d25c9d427753ff752fa72930
Dor <dor.moshe@post.idc.ac.il>
2018-03-06 19:09:54 +0200

    initialized

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelTime;
+    public int score;
+    public float startTime;
+
+    public GameObject[] levels;
+
+	// Use this for initialization
+	void Start () {
+        this.levelTime = 5;
+        this.score = 0;
+        this.startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time - this.startTime > levelTime)
+        {
+            endCurrentLevel();
+        }
+		
+	}
+
+    void endCurrentLevel()
+    {
+        return;
+    }
+
+    //LEVELS ARE SCENES
+
+    void StartNewLevel()
+    {
+        return;
+    }
+}
